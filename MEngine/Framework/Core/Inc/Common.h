94b069a2e32a29c7457186602535d55acda5009a
cfmeans123 <123025985+cfmeans123@users.noreply.github.com>
2024-01-10 21:55:59 -0800

    MEngine
    First Commit - Initial engine setup

---
+++ MEngine/Framework/Core/Inc/Common.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#define WIN32_MEAN_AND_LEAN
+#define NOMINMAX
+
+//win32 headers
+#include <objbase.h>
+#include <Windows.h>
+
+//std headers
+#include <algorithm>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <cstdio>
+#include <cstdlib>
+#include <cstdint>
+#include <filesystem>
+#include <functional>
+#include <list>
+#include <map>
+#include <memory>
+#include <optional>
+#include <string>
+#include <unordered_map>
+#include <variant>
+#include <vector>

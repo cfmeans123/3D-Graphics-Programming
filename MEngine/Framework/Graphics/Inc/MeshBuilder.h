ae8d1b0c9cc324a1d2cde2bec275f16bd29320ce
cfmeans123 <123025985+cfmeans123@users.noreply.github.com>
2024-02-14 20:26:06 -0800

    cleaning_for_push

--- MEngine/Framework/Graphics/Inc/MeshBuilder.h
+++ MEngine/Framework/Graphics/Inc/MeshBuilder.h
@@ -13,5 +13,6 @@ namespace MEngine::Graphics
         static MeshPC CreateVerticalPlanePC(uint32_t numRows, uint32_t numCols, float spacing);
         static MeshPC CreateHorizontalPlanePC(uint32_t numRows, uint32_t numCols, float spacing);
 
+        void CreatePlaneIndices(std::vector<uint32_t> indices, )
     };
 }
